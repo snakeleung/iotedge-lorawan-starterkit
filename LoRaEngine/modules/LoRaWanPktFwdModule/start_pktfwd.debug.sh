0d2b71272a0e98b1e74e719a34d8825e88a13298
fbeltrao <fbeltrao@windowslive.com>
2018-11-23 19:00:55 +0100

    Add local debugging w/ pkt forwarder in edge device

---
+++ LoRaEngine/modules/LoRaWanPktFwdModule/start_pktfwd.debug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$REGION" ]; then
+     echo "No region detected in environment variables, defaulting to EU" 
+else
+    if [ "$REGION" == "US" ]; then
+        echo "US region detected." 
+        cp global_conf.us.json global_conf.json 
+    else
+        if [ "$REGION" == "EU" ]; then
+            echo "EU region detected." 
+            cp global_conf.eu.json global_conf.json 
+        fi
+    fi
+fi
+
+sed -i -e "s/172.17.0.1/$NETWORK_SERVER/g" local_conf.json
+
+./reset_lgw.sh start $RESET_PIN
+./lora_pkt_fwd
